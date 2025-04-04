d67e99c3802e95d940163e455650d0a8e2634240
NikoP93 <nipe0001@stud.kea.dk>
2025-04-04 11:00:06 +0200

    Main Menu added

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from board import print_board, create_board
 from input_handler import player_input,computer_move
 from win_checker import check_win_conditions
-from history import save_history
+from history import save_history,read_history
+
+def main_menu():
+    while True:
+        try:
+            menu = int(input(""" 
+                            1. Play Tic Tac Toe
+                            2. View History
+                            3. Quit \n """))
+            if menu == 1:
+                play_game()
+            elif menu == 2:
+                read_history()
+            elif menu == 3:
+                print('Thanks for playing')
+                break
+        except ValueError:
+            print("Invalid Input, please use 1, 2 or 3")
 
 def play_game():
+
     board = create_board()
     current_player = "X"
     move_count = 0
@@ -32,11 +50,11 @@ def play_game():
 
         # Swap player
         current_player = "O" if current_player == "X" else "X"
-        
-    # Play again option
+
+        # Play again option
     should_continue = input("Play again? (y/n): ").lower()
     if should_continue == "y":
         play_game()
 
-play_game()
-       
+if __name__ == "__main__":
+    main_menu()
--- input_handler.py
+++ input_handler.py
@@ -3,14 +3,15 @@ from win_checker import check_win_conditions
 
 def player_input(board, currentPlayer):
     while True:
+        try:
             inp = int(input("Enter a number 1-9: "))
             if 1 <= inp <= 9 and board[inp - 1] == ' ':
                 board[inp - 1] = currentPlayer
                 break
             elif 1 <= inp <= 9:
                 print('Field is already taken')
-            else:
-                print('Invalid number')
+        except ValueError:
+            print('Invalid number, please use 1-9')
 
 def computer_move(board):
     # 1. Check if computer can win in next move
