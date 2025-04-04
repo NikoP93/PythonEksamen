11a61fed0ca94e2b9f105736fcff279a3eccaa90
NikoP93 <nipe0001@stud.kea.dk>
2025-04-04 12:56:50 +0200

    changed up in the read history function

--- history.py
+++ history.py
@@ -5,5 +5,14 @@ def save_history(text):
 
 def read_history():
     with open('score.txt','r') as f:
-        content = f.read()
-        print(" Game History:\n" + content if content else "No history yet.")
+        content = f.readlines()
+        player_wins = [line for line in content if "Player won" in line]
+        computer_wins = [line for line in content if "Computer won" in line]
+        draw = [line for line in content if "draw" in line]
+        print(f""" 
+            Game stats
+            ------------------
+            Player wins: {len(player_wins)}
+            Computer wins: {len(computer_wins)}
+            Draws: {len(draw)}
+""")
